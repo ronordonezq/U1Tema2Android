bcbb960cad58edb2ee0e78f9e4e069722a319642
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-09 20:34:13 -0500

    Completado fragmentTabhost

---
+++ app/src/main/java/com/example/u1tema2android/FragmentTabhost/Tab1.java
@@ -0,0 +1,24 @@
+package com.example.u1tema2android.FragmentTabhost;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.u1tema2android.R;
+
+public class Tab1 extends Fragment {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.tab1, container, false);
+  }
+}
+
---
+++ app/src/main/java/com/example/u1tema2android/FragmentTabhost/Tab3.java
@@ -0,0 +1,23 @@
+package com.example.u1tema2android.FragmentTabhost;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.u1tema2android.R;
+
+public class Tab3 extends Fragment {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.activity_intenciones, container, false);
+  }
+}
